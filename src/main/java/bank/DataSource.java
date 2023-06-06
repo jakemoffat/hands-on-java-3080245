b8c48f0ce743337d79ce688d573e611bff5d9b28
Jake Moffat <117701842+jakemoffat@users.noreply.github.com>
2023-06-06 14:23:45 +0000

    Vid02_02 Edits

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -1,7 +1,10 @@
 package bank;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class DataSource {
   // This class will handle connecting to the database
@@ -14,7 +17,7 @@ public class DataSource {
     try {
       connection = DriverManager.getConnection(db_file);
       System.out.println("Connection Granted");
-    } catch(SQLException exception) {
+    } catch (SQLException exception) {
       exception.printStackTrace();
     }
 
@@ -22,7 +25,33 @@ public class DataSource {
 
   }
 
+  public static Customer getCustomer(String username) {
+    // This method will accept the customer username
+    String sql = "select * from customers where username = ?";
+    Customer customer = null;
+
+    try (Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setString(1, username);
+      try (ResultSet resultSet = statement.executeQuery()) {
+        customer = new Customer(
+            resultSet.getInt("id"),
+            resultSet.getString("name"),
+            resultSet.getString("username"),
+            resultSet.getString("password"),
+            resultSet.getInt("account_id"));
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return customer;
+  }
+
   public static void main(String[] args) {
-    connect();
+    //connect();
+    Customer customer = getCustomer("twest8o@friendfeed.com");
+    System.out.println(customer.getName());
   }
 }
